6700a3ea845bfef91231bf338c78d2c9c4b3bd2c
Stéphane Lepin <stephane.lepin@gmail.com>
2018-03-16 22:37:36 +0100

    Add libwebsockets dependency

---
+++ src/Transport/WebsocketsServer.h
@@ -0,0 +1,17 @@
+
+#ifndef OBS_WEBSOCKET_WEBSOCKETSSERVER_H
+#define OBS_WEBSOCKET_WEBSOCKETSSERVER_H
+
+
+#include <deps/libwebsockets/lib/private-libwebsockets.h>
+
+class WebsocketsServer {
+public:
+    WebsocketsServer();
+    ~WebsocketsServer();
+private:
+
+};
+
+
+#endif //OBS_WEBSOCKET_WEBSOCKETSSERVER_H
