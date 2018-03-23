c05eb5507e4050c6bb25e4cc67465e1f5505bb4e
Stéphane Lepin <stephane.lepin@gmail.com>
2018-03-23 15:59:32 +0100

    Macros for method creation

--- src/Rpc/Methods/RpcMethods.h
+++ src/Rpc/Methods/RpcMethods.h
@@ -1,21 +1,7 @@
 #pragma once
 
-#include <QJsonArray>
-
 #include "src/Rpc/RpcMethod.h"
 #include "src/Utils.h"
 #include "src/obs-websocket.h"
 
-class GetVersion : public RpcMethod {
-  public:
-	  const RpcResponse handle(const RpcRequest& request) override
-	  {
-		  QString obsVersion = Utils::OBSVersionString();
-
-		  QVariantHash params;
-		  params.insert("pluginVersion", OBS_WEBSOCKET_VERSION);
-		  params.insert("programVersion", obsVersion);
-
-		  return RpcResponse::ok(request, params);
-	  }
-};
+RPCMETHOD_BOILERPLATE(GetVersion)
